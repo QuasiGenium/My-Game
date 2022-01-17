614f679ed413cb9478c9fca851e043b830055425
QuasiGenium <Anotherzerosun@gmail.com>
2022-01-17 07:00:52 +0300

    Начало работы с персонажем

--- main.py
+++ main.py
@@ -22,15 +22,36 @@ all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 enemy_group = pygame.sprite.Group()
-player_image = load_image('testhero.png')
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
+    def __init__(self, sheet, columns, rows, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
-        self.image = player_image
         self.pos_x, self.pos_y = pos_x, pos_y
-        self.rect = self.image.get_rect().move(pos_x, pos_y)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(pos_x, pos_y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+    def stop(self, k):
+        pass
+
+    def walk_an(self, k):
+        pass
 
     def move(self, ke):
         if ke == 'w' or ke == 'ц':
@@ -46,9 +67,10 @@ class Player(pygame.sprite.Sprite):
 
 
 def main():
-    hero = Player(500, 300)
+    hero = Player(load_image('beg.png'), 10, 8, 500, 300)
     run = True
     key = ''
+    p = 0
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -61,6 +83,14 @@ def main():
                 if event.unicode == key:
                     key = ''
         hero.move(key)
+        '''
+        if p == 1:
+            hero.update()
+            p += 1
+        else:
+            p += 1
+            p %= 6
+        '''
         screen.fill((0, 0, 0))
         all_sprites.draw(screen)
         enemy_group.draw(screen)
